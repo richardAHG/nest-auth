b646015a16ada1245984ad7f2ab1362e04f8905e
Richard Huáman Gómez <richard21hg92@gmail.com>
2022-05-20 12:51:47 -0500

    encriptacion del password usando bcrypt

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -3,6 +3,8 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
 import { RegisterUserDto } from './dto/register-user.dto';
 import { User } from './user.entity';
 
@@ -10,7 +12,11 @@ import { User } from './user.entity';
 export class UsersRepository extends Repository<User> {
   async createUser(registerUser: RegisterUserDto): Promise<void> {
     const { name, email, password } = registerUser;
-    const user = this.create({ name, email, password });
+
+    //hash de password
+    const salt = await bcrypt.genSalt();
+    const hashpassword = await bcrypt.hash(password, salt);
+    const user = this.create({ name, email, password: hashpassword });
 
     try {
       await this.save(user);
